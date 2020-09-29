7666fb50d88934c5875824d2694e07e4fa549c38
eternalsad <sadikcartman@gmail.com>
2020-09-29 22:08:16 +0600

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    if (array == null || array.length == 0) return 0;
+    let min = Number.POSITIVE_INFINITY;
+    array.forEach((element) => {
+        min = element < min ? element : min;
+    });
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == null || array.length == 0) return 0;
+    return array.reduce((a, b) => {
+        if (a > b) return a;
+        else return b;
+    });
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == null || array.length == 0) return 0;
+    return array.reduce((a, b) => a + b) / array.length;
+};
